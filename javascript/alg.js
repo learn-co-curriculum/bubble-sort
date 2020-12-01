27541fdf8d571c3d3844cdf5d53b4a13daa2e65d
Rupa <rupa211984@gmail.com>
2020-11-30 16:42:34 -0800

    Readme

---
+++ javascript/alg.js
@@ -0,0 +1,19 @@
+function methodName(arg) {
+  // type your code here
+}
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: 3");
+  console.log(methodName([2, 1, 3, 3, 2]));
+
+  console.log("");
+
+  console.log("Expecting: -1");
+  console.log(methodName([1, 2, 3, 4]));
+}
+
+module.exports = methodName;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
