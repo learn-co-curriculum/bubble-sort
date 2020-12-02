655cab9fbc836e84a5d40089b4a04edba4e0a5c2
Rupa <rupa211984@gmail.com>
2020-12-02 12:47:04 -0800

    Add arrows

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -5,22 +5,22 @@ function bubbleSort(arr) {
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [1, 2, 3, 4]");
-  console.log(bubbleSort([3, 2, 1, 4]));
+  console.log("=>", bubbleSort([3, 2, 1, 4]));
 
   console.log("");
 
   console.log("Expecting: [1, 2, 3]");
-  console.log(bubbleSort([1, 2, 3]));
+  console.log("=>", bubbleSort([1, 2, 3]));
 
   console.log("");
 
   console.log("Expecting: []");
-  console.log(bubbleSort([]));
+  console.log("=>", bubbleSort([]));
 
   console.log("");
 
   console.log("Expecting: [1, 2, 3]");
-  console.log(bubbleSort([2, 3, 1]));
+  console.log("=>", bubbleSort([2, 3, 1]));
 }
 
 module.exports = bubbleSort;
--- ruby/bubble_sort.rb
+++ ruby/bubble_sort.rb
@@ -4,21 +4,25 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: [1, 2, 3, 4]"
+  print "=> "
   print bubble_sort([3, 2, 1, 4])
 
   puts
 
   puts "Expecting: [1, 2, 3]"
+  print "=> "
   print bubble_sort([1, 2, 3])
 
   puts
 
   puts "Expecting: []"
+  print "=> "
   print bubble_sort([])
 
   puts
 
   puts "Expecting: [1, 2, 3]"
+  print "=> "
   print bubble_sort([2, 3, 1])
 
   # Don't forget to add your own!
